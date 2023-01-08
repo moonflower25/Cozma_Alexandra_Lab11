c66caa6d322bd759e79ece3d03e649b0587f5f50
Alexandra Cozma <ale.cozma2526@gmail.com>
2023-01-08 17:40:35 +0200

    Add project files.

---
+++ Cozma_Alexandra_Lab11/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Cozma_Alexandra_Lab11.Data;
+using Cozma_Alexandra_Lab11.Models;
+
+namespace Cozma_Alexandra_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Cozma_Alexandra_Lab11/Data/IRestService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cozma_Alexandra_Lab11.Models;
+
+
+namespace Cozma_Alexandra_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+
+        Task DeleteShopListAsync(int id);
+    }
+
+}
